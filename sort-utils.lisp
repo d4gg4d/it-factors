278eabfa1e6b2f2ee6ae33757c683094924ce7e3
Sami Airaksinen <samiaira@gmail.com>
2013-07-01 21:44:02 +0300

    reversing sorting to be ascending

--- sort-utils.lisp
+++ sort-utils.lisp
@@ -6,6 +6,8 @@
   (if (null (cdr list))
       list
       (let ((less '()) (greater '()) (pivot (car list)))
-	(mapc #'(lambda (x) (if (<= x pivot) (push x greater) (push x less))) (cdr list))
+	(mapc #'(lambda (x) (if (> x pivot) (push x greater) (push x less))) (cdr list))
 	(append (quicksort less) (cons pivot (quicksort greater))))))
 
+(quicksort '(1 3 10 5435 2 94 7 4 1))
+;; (:ok "=> (1 1 2 3 4 7 10 94 5435)")
