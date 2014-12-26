6b92dc0cb8c7c56b0adfa7aa863ad43d64792e7b
Sami Airaksinen <samiaira@gmail.com>
2014-12-26 16:48:30 +0200

    improving syntax, more idiomatic CL

--- day-of-the-week.lisp
+++ day-of-the-week.lisp
@@ -8,60 +8,27 @@
 (defpackage :org.d4gg4d.day-of-the-week
   (:use :common-lisp))
 
-(defun partial (f &rest args)
-  "currying function"
-  (lambda (&rest more-args)
-    (apply f (append args more-args))))
+(defvar *month-to-code*
+  '(1 4 4 0 2 5 0 3 6 1 4 6))
 
-(defun -put-entry-to (hash-table key value)
-  "set entry set to hash table"
-  (setf (gethash key hash-table) value))
-
-(defun put-entry-to (hash-table key-value)
-  (-put-entry-to hash-table (car key-value) (cadr key-value)))
-
-(defun last-two-digits (year)
-  (nth-value 1 (truncate year 100)))
-
-(defvar month-to-code)
-(setq month-to-code (make-hash-table))
-(mapcar (partial 'put-entry-to month-to-code) '((1 1)
-						(2 4)
-						(3 4)
-						(4 0)
-						(5 2)
-						(6 5)
-						(7 0)
-						(8 3)
-						(9 6)
-						(10 1)
-						(11 4)
-						(12 6)))
-
-(defvar code-to-day)
-(setq code-to-day (make-hash-table))
-(mapcar (partial 'put-entry-to code-to-day) '((0 "Saturday")
-					      (1 "Sunday")
-					      (2 "Monday")
-					      (3 "Tuesday")
-					      (4 "Wednesday")
-					      (5 "Thursday")
-					      (6 "Friday")))
-
-(defun century-code (century)
-  (- 6 (* 2 (mod century 4))))
+(defvar *code-to-day*
+  '("Saturday" "Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday"))
 
 (defun resolve-year-code (year)
-  (let* ((century-value (century-code (floor year 100)))
-	 (last-digits (last-two-digits year))
-	 (digits-division-value (floor last-digits 4)))
-    (mod (+ century-value last-digits digits-division-value) 7)))
+  (flet ((century-code (century)
+		       (- 6 (* 2 (mod century 4))))
+	 (last-two-digits (year)
+			  (rem year 100)))
+	(let* ((century-value (century-code (floor year 100)))
+	       (last-digits (last-two-digits year))
+	       (digits-division-value (floor last-digits 4)))
+	  (mod (+ century-value last-digits digits-division-value) 7))))
 
 (defun resolve-leap-year-code (year month)
   (if (and (zerop (mod year 4)) (> 3 month)) -1 0))
 
-(defun fetch-month-code (month)
-  (gethash month month-to-code))
+(defun resolve-month-code (month)
+  (nth (1- month) *month-to-code*))
 
 (defun resolve-day-code (year-code leap-year-code month-code day)
   (mod (+ year-code leap-year-code month-code day) 7))
@@ -69,10 +36,10 @@
 (defun day-of-the-week (year month day)
   "returns the day of the week for given date by human calculatable rules"
   (let* ((year-code (resolve-year-code year))
-	 (month-code (fetch-month-code month))
+	 (month-code (resolve-month-code month))
 	 (leap-year-code (resolve-leap-year-code year month))
-	 (day-code (resolve-day-code year-code leap-year-code month-code day)))
-    (gethash day-code code-to-day)))
+	 (day-code (resolve-day-code year-code leap-year-code month-code day))) ;
+    (nth day-code *code-to-day*)))
 
 (day-of-the-week 2014 12 24)
 ;; => "Wednesday"
